ac2caf4600013bb3b7459670f34ac20758ef927c
viveksgonal <gonalvivek7@gmail.com>
2022-02-20 08:21:26 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,32 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+import React from 'react';
+import {createStore, combineReducers} from 'redux';
+import {Provider} from 'react-redux';
+import productsReducer from './store/reducers/products';
+import cartReducer from './store/reducers/cart';
+import ordersReducer from './store/reducers/orders';
+import ShopNavigator from './navigation/ShopNavigator';
+
+const rootReducer = combineReducers({
+  products: productsReducer,
+  cart: cartReducer,
+  orders: ordersReducer,
+});
+
+const store = createStore(rootReducer);
+
+const App = () => {
+  return (
+    <Provider store={store}>
+      <ShopNavigator />
+    </Provider>
+  );
+};
+
+export default App;
